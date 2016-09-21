ad2fc5dee5f92910f612b0d7bc19bbd682dace19
xhjlee <xhjlee@bequs.co.kr>
2016-09-21 16:36:09 +0900

    최초 설정및 테스트

---
+++ src/main/java/com/example/test/TestController.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by bequs-xhjlee on 2016-09-21.
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello test world";
+    }
+}
